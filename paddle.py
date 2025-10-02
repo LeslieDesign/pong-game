dc954aff7cd9c1bcbabd90b8d17048c78cff803b
Leslie Middleton <lesliemddltn@gmail.com>
2025-10-02 14:34:37 -0400

    Update scoring logic and improve README

--- paddle.py
+++ paddle.py
@@ -2,7 +2,7 @@ from turtle import Turtle
 
 
 class Paddle(Turtle):
-    def __init__(self, position):
+    def __init__(self, position: object) -> None:
         super().__init__()
         self.shape("square")
         self.shapesize(stretch_wid=5, stretch_len=1)
